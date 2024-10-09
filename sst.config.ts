1a34e3975ce5778c98f82c478d8459a00f07cf93
Crhistian Caraballo <cristianjcj35@gmail.com>
2024-10-09 09:44:51 -0500

    docs(pr): fix format

--- sst.config.ts
+++ sst.config.ts
@@ -1,35 +1,14 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
-const domain = 'concertplaza.tv';
-
-const getDomain = (env: string, prefix = '') => {
-  switch (env) {
-    case 'prod':
-      return `${prefix}${domain}`;
-    default:
-      return `${prefix}${env}.${domain}`;
-  }
-};
-
 export default $config({
   app(input) {
     return {
-      name: 'ctp-dxp-fe-investors',
-      removal: 'retain',
-      home: 'aws',
+      name: "my-app",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
     };
   },
   async run() {
-    new sst.aws.Nextjs('InvestorSite', {
-      // warm: $app.stage === 'prod' ? 1 : 0,
-      warm: 1,
-      domain: {
-        name: getDomain($app.stage, 'investors.'),
-        aliases: [getDomain($app.stage, 'www.investors.')],
-        dns: sst.aws.dns({
-          zone: 'Z07436663RA555S22OI2I',
-        }),
-      },
-    });
+    new sst.aws.Nextjs("MyWeb");
   },
 });
